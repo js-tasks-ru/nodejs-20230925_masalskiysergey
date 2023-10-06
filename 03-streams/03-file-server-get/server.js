5fa652450d1671167ea08480549dee038c3e2ad2
Sergey Masalski <masalskiysergey@gmail.com>
2023-10-06 13:26:44 +0300

    finished 03-streams/03-file-server-get

--- 03-streams/03-file-server-get/server.js
+++ 03-streams/03-file-server-get/server.js
@@ -1,24 +1,42 @@
 const url = require('url');
-const http = require('http');
+const { Server } = require('http');
 const path = require('path');
+const fs = require('node:fs');
 
-const server = new http.Server();
+const server = new Server();
 
 server.on('request', (req, res) => {
-  const url = new URL(req.url, `http://${req.headers.host}`);
-  const pathname = url.pathname.slice(1);
+    const url = new URL(req.url, `http://${req.headers.host}`);
+    const pathname = url.pathname.slice(1);
 
-  const filepath = path.join(__dirname, 'files', pathname);
+    const filepath = path.join(__dirname, 'files', pathname);
+    const readFile = fs.createReadStream(filepath);
 
-  switch (req.method) {
-    case 'GET':
+    switch (req.method) {
+        case 'GET':
+            readFile.pipe(res);
 
-      break;
+            readFile.on('error', err => {
+                if (err.code === 'ENOENT') {
+                    if (pathname.split('/').length > 1) {
+                        res.statusCode = 400;
+                        return res.end('Invalid path');
+                    }
 
-    default:
-      res.statusCode = 501;
-      res.end('Not implemented');
-  }
+                    res.statusCode = 404;
+                    return res.end('Page not found');
+                }
+
+                res.statusCode = 500;
+                res.end('Internal Server Error');
+            });
+
+            break;
+
+        default:
+            res.statusCode = 501;
+            res.end('Not implemented');
+    }
 });
 
 module.exports = server;
