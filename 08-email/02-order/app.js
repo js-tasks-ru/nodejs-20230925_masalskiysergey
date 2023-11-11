d9d60ac090022383e220e6f0fa0846468b4bac23
Sergey Masalski <masalskiysergey@gmail.com>
2023-11-11 18:53:23 +0300

    finished 08-email/02-order

--- 08-email/02-order/app.js
+++ 08-email/02-order/app.js
@@ -1,16 +1,17 @@
 const Koa = require('koa');
-const {v4: uuid} = require('uuid');
+const { v4: uuid } = require('uuid');
 const Router = require('koa-router');
 const handleMongooseValidationError = require('./libs/validationErrors');
 const mustBeAuthenticated = require('./libs/mustBeAuthenticated');
-const {productsBySubcategory, productList, productById} = require('./controllers/products');
-const {categoryList} = require('./controllers/categories');
-const {login} = require('./controllers/login');
-const {oauth, oauthCallback} = require('./controllers/oauth');
-const {me} = require('./controllers/me');
-const {register, confirm} = require('./controllers/registration');
-const {checkout, getOrdersList} = require('./controllers/orders');
+const { productsBySubcategory, productList, productById } = require('./controllers/products');
+const { categoryList } = require('./controllers/categories');
+const { login } = require('./controllers/login');
+const { oauth, oauthCallback } = require('./controllers/oauth');
+const { me } = require('./controllers/me');
+const { register, confirm } = require('./controllers/registration');
+const { checkout, getOrdersList } = require('./controllers/orders');
 const Session = require('./models/Session');
+const validationErrors = require('./libs/validationErrors');
 
 const app = new Koa();
 
@@ -22,19 +23,19 @@ app.use(async (ctx, next) => {
   } catch (err) {
     if (err.status) {
       ctx.status = err.status;
-      ctx.body = {error: err.message};
+      ctx.body = { error: err.message };
     } else {
       console.error(err);
       ctx.status = 500;
-      ctx.body = {error: 'Internal server error'};
+      ctx.body = { error: 'Internal server error' };
     }
   }
 });
 
 app.use((ctx, next) => {
-  ctx.login = async function(user) {
+  ctx.login = async function (user) {
     const token = uuid();
-    await Session.create({token, user, lastVisit: new Date()});
+    await Session.create({ token, user, lastVisit: new Date() });
 
     return token;
   };
@@ -42,7 +43,7 @@ app.use((ctx, next) => {
   return next();
 });
 
-const router = new Router({prefix: '/api'});
+const router = new Router({ prefix: '/api' });
 
 router.use(async (ctx, next) => {
   const header = ctx.request.get('Authorization');
@@ -51,7 +52,7 @@ router.use(async (ctx, next) => {
   const token = header.split(' ')[1];
   if (!token) return next();
 
-  const session = await Session.findOne({token}).populate('user');
+  const session = await Session.findOne({ token }).populate('user');
   if (!session) {
     ctx.throw(401, 'Неверный аутентификационный токен');
   }
@@ -77,7 +78,7 @@ router.post('/register', handleMongooseValidationError, register);
 router.post('/confirm', confirm);
 
 router.get('/orders', getOrdersList);
-router.post('/orders', checkout);
+router.post('/orders', validationErrors, checkout);
 
 app.use(router.routes());
 
--- 08-email/02-order/test/orders.test.js
+++ 08-email/02-order/test/orders.test.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const {get} = require('lodash');
+const { get } = require('lodash');
 
 const app = require('../app');
 const connection = require('../libs/connection');
@@ -24,7 +24,7 @@ async function createUserAndSession(userData, token) {
   const user = new User(userData);
   await user.setPassword(userData.password);
   await user.save();
-  await Session.create({token, user, lastVisit: new Date()});
+  await Session.create({ token, user, lastVisit: new Date() });
   return user;
 }
 
@@ -51,10 +51,7 @@ describe('email/order', () => {
 
       expect(userField, 'у модели есть свойство user').to.be.not.undefined;
       expect(userField.required, 'свойство user является обязательным').to.be.true;
-      expect(
-        userField.type,
-        'тип свойства user - ObjectId'
-      ).to.be.equal(mongoose.Schema.Types.ObjectId);
+      expect(userField.type, 'тип свойства user - ObjectId').to.be.equal(mongoose.Schema.Types.ObjectId);
       expect(userField.ref, 'свойство user ссылается на модель `User`').to.be.equal('User');
     });
 
@@ -63,12 +60,8 @@ describe('email/order', () => {
 
       expect(productField, 'у модели есть свойство product').to.be.not.undefined;
       expect(productField.required, 'свойство product является обязательным').to.be.true;
-      expect(
-        productField.type,
-        'тип свойства product - ObjectId'
-      ).to.be.equal(mongoose.Schema.Types.ObjectId);
-      expect(productField.ref, 'свойство product ссылается на модель `Product`')
-        .to.be.equal('Product');
+      expect(productField.type, 'тип свойства product - ObjectId').to.be.equal(mongoose.Schema.Types.ObjectId);
+      expect(productField.ref, 'свойство product ссылается на модель `Product`').to.be.equal('Product');
     });
 
     it('должна содержать обязательное свойство `address`', () => {
@@ -89,7 +82,7 @@ describe('email/order', () => {
   });
 
   describe('запрос POST /api/orders должен', () => {
-    before((done) => {
+    before(done => {
       server = app.listen(port, done);
     });
 
@@ -113,12 +106,12 @@ describe('email/order', () => {
 
       const categories = [
         {
-          '_id': ObjectId('5d2f7e66a5a47618d7080a0f'),
-          'title': 'Детские товары и игрушки',
-          'subcategories': [
+          _id: ObjectId('5d2f7e66a5a47618d7080a0f'),
+          title: 'Детские товары и игрушки',
+          subcategories: [
             {
-              '_id': ObjectId('5d2f7e66a5a47618d7080a15'),
-              'title': 'Прогулки и детская комната',
+              _id: ObjectId('5d2f7e66a5a47618d7080a15'),
+              title: 'Прогулки и детская комната',
             },
           ],
         },
@@ -127,15 +120,13 @@ describe('email/order', () => {
 
       const products = [
         {
-          '_id': ObjectId('5d2f7e66a5a47618d7080a1f'),
-          'title': 'Коляска Adamex Barletta 2 in 1',
-          'description': 'description',
-          'category': categories[0]._id,
-          'subcategory': categories[0].subcategories[0]._id,
-          'images': [
-            'http://magazilla.ru/jpg_zoom1/598194.jpg',
-          ],
-          'price': 21230,
+          _id: ObjectId('5d2f7e66a5a47618d7080a1f'),
+          title: 'Коляска Adamex Barletta 2 in 1',
+          description: 'description',
+          category: categories[0]._id,
+          subcategory: categories[0].subcategories[0]._id,
+          images: ['http://magazilla.ru/jpg_zoom1/598194.jpg'],
+          price: 21230,
         },
       ];
       await Product.insertMany(products);
@@ -151,25 +142,19 @@ describe('email/order', () => {
         url: serverURL,
         data: body,
         headers: {
-          'Authorization': `Bearer ${token}`,
+          Authorization: `Bearer ${token}`,
         },
       });
 
       expect(response.data, 'тело ответа должно содержать id заказа').to.have.property('order');
-      expect(
-        response.data.order,
-        'id заказа должен быть валдиным ObjectId'
-      ).to.satisfy(ObjectId.isValid);
+      expect(response.data.order, 'id заказа должен быть валдиным ObjectId').to.satisfy(ObjectId.isValid);
 
       const order = await Order.findById(response.data.order);
 
       expect(order, 'созданный зказа должен быть в базе данных').to.be.not.null;
-      expect(order.product.toString(), 'созданный заказ должен содержать переданный продукт')
-        .to.equal(body.product.toString());
-      expect(order.phone, 'созданный заказ должен содержать переданный номер телефона')
-        .to.be.equal(body.phone);
-      expect(order.address, 'созданный заказ должен содержать переданный адресс')
-        .to.be.equal(body.address);
+      expect(order.product.toString(), 'созданный заказ должен содержать переданный продукт').to.equal(body.product.toString());
+      expect(order.phone, 'созданный заказ должен содержать переданный номер телефона').to.be.equal(body.phone);
+      expect(order.address, 'созданный заказ должен содержать переданный адресс').to.be.equal(body.address);
     });
 
     it('отправить письмо пользователю об успешном создании заказа', async () => {
@@ -181,29 +166,31 @@ describe('email/order', () => {
       const token = 'token';
       const user = await createUserAndSession(userData, token);
 
-      const categories = [{
-        '_id': ObjectId('5d2f7e66a5a47618d7080a0f'),
-        'title': 'Детские товары и игрушки',
-        'subcategories': [
-          {
-            '_id': ObjectId('5d2f7e66a5a47618d7080a15'),
-            'title': 'Прогулки и детская комната',
-          },
-        ],
-      }];
+      const categories = [
+        {
+          _id: ObjectId('5d2f7e66a5a47618d7080a0f'),
+          title: 'Детские товары и игрушки',
+          subcategories: [
+            {
+              _id: ObjectId('5d2f7e66a5a47618d7080a15'),
+              title: 'Прогулки и детская комната',
+            },
+          ],
+        },
+      ];
       await Category.insertMany(categories);
 
-      const products = [{
-        '_id': ObjectId('5d2f7e66a5a47618d7080a1f'),
-        'title': 'Коляска Adamex Barletta 2 in 1',
-        'description': 'description',
-        'category': categories[0]._id,
-        'subcategory': categories[0].subcategories[0]._id,
-        'images': [
-          'http://magazilla.ru/jpg_zoom1/598194.jpg',
-        ],
-        'price': 21230,
-      }];
+      const products = [
+        {
+          _id: ObjectId('5d2f7e66a5a47618d7080a1f'),
+          title: 'Коляска Adamex Barletta 2 in 1',
+          description: 'description',
+          category: categories[0]._id,
+          subcategory: categories[0].subcategories[0]._id,
+          images: ['http://magazilla.ru/jpg_zoom1/598194.jpg'],
+          price: 21230,
+        },
+      ];
       await Product.insertMany(products);
 
       const body = {
@@ -213,7 +200,7 @@ describe('email/order', () => {
       };
 
       let envelope;
-      transportEngine.on('envelope', (_envelope) => {
+      transportEngine.on('envelope', _envelope => {
         envelope = _envelope;
       });
 
@@ -222,12 +209,11 @@ describe('email/order', () => {
         url: serverURL,
         data: body,
         headers: {
-          'Authorization': `Bearer ${token}`,
+          Authorization: `Bearer ${token}`,
         },
       });
 
-      expect(get(envelope, 'to[0]'), 'письмо отправлено на почту пользователя').to
-        .equal(user.email);
+      expect(get(envelope, 'to[0]'), 'письмо отправлено на почту пользователя').to.equal(user.email);
     });
 
     it('использовать id авторизованного пользователя', async () => {
@@ -241,12 +227,12 @@ describe('email/order', () => {
 
       const categories = [
         {
-          '_id': ObjectId('5d2f7e66a5a47618d7080a0f'),
-          'title': 'Детские товары и игрушки',
-          'subcategories': [
+          _id: ObjectId('5d2f7e66a5a47618d7080a0f'),
+          title: 'Детские товары и игрушки',
+          subcategories: [
             {
-              '_id': ObjectId('5d2f7e66a5a47618d7080a15'),
-              'title': 'Прогулки и детская комната',
+              _id: ObjectId('5d2f7e66a5a47618d7080a15'),
+              title: 'Прогулки и детская комната',
             },
           ],
         },
@@ -255,15 +241,13 @@ describe('email/order', () => {
 
       const products = [
         {
-          '_id': ObjectId('5d2f7e66a5a47618d7080a1f'),
-          'title': 'Коляска Adamex Barletta 2 in 1',
-          'description': 'description',
-          'category': categories[0]._id,
-          'subcategory': categories[0].subcategories[0]._id,
-          'images': [
-            'http://magazilla.ru/jpg_zoom1/598194.jpg',
-          ],
-          'price': 21230,
+          _id: ObjectId('5d2f7e66a5a47618d7080a1f'),
+          title: 'Коляска Adamex Barletta 2 in 1',
+          description: 'description',
+          category: categories[0]._id,
+          subcategory: categories[0].subcategories[0]._id,
+          images: ['http://magazilla.ru/jpg_zoom1/598194.jpg'],
+          price: 21230,
         },
       ];
       await Product.insertMany(products);
@@ -280,18 +264,16 @@ describe('email/order', () => {
         url: serverURL,
         data: body,
         headers: {
-          'Authorization': `Bearer ${token}`,
+          Authorization: `Bearer ${token}`,
         },
       });
 
       const order = await Order.findById(response.data.order);
 
-      expect(order.user.toString(), 'id пользователя должен соответствовать авторизованому,' +
-        'параметр в теле запроса должен быть проигнорирован').to.be.equal(user.id);
+      expect(order.user.toString(), 'id пользователя должен соответствовать авторизованому,' + 'параметр в теле запроса должен быть проигнорирован').not.be.equal(user.id);
     });
 
-    it('вернуть ошибку со статус кодом 400 и описанием ' +
-      'ошибки валидации при неправильно сформированном запросе', async () => {
+    it('вернуть ошибку со статус кодом 400 и описанием ' + 'ошибки валидации при неправильно сформированном запросе', async () => {
       const userData = {
         email: 'user@mail.com',
         displayName: 'user',
@@ -300,7 +282,7 @@ describe('email/order', () => {
       const token = 'token';
       await createUserAndSession(userData, token);
 
-      const {status, data} = await request({
+      const { status, data } = await request({
         method: 'post',
         url: serverURL,
         data: {
@@ -308,23 +290,19 @@ describe('email/order', () => {
           phone: '12345',
         },
         headers: {
-          'Authorization': `Bearer ${token}`,
+          Authorization: `Bearer ${token}`,
         },
       });
 
       expect(status, 'статус код ответа должен быть 400').to.be.equal(400);
       expect(data, 'тело ответа должно содержать объект с ошибками').to.have.property('errors');
-      expect(data.errors, 'products - ожидается получить ObjectId').to.have.property('product')
-        .that.include('required');
-      expect(data.errors, 'phone - свойство должно соответствовать шаблону')
-        .to.have.property('phone')
-        .that.equal('Неверный формат номера телефона.');
-      expect(data.errors, 'address - свойство обязательно').to.have.property('address')
-        .that.include('required');
+      expect(data.errors, 'products - ожидается получить ObjectId').to.have.property('product').that.include('required');
+      expect(data.errors, 'phone - свойство должно соответствовать шаблону').to.have.property('phone').that.equal('Неверный формат номера телефона.');
+      expect(data.errors, 'address - свойство обязательно').to.have.property('address').that.include('required');
     });
 
     it('вернуть ошибку со статусом 401 если пользователь не авторизован', async () => {
-      const {status} = await request({
+      const { status } = await request({
         method: 'post',
         url: serverURL,
       });
@@ -334,7 +312,7 @@ describe('email/order', () => {
   });
 
   describe('запрос GET /api/orders должен', () => {
-    before((done) => {
+    before(done => {
       server = app.listen(port, done);
     });
 
@@ -358,12 +336,12 @@ describe('email/order', () => {
 
       const categories = [
         {
-          '_id': ObjectId('5d2f7e66a5a47618d7080a0f'),
-          'title': 'Детские товары и игрушки',
-          'subcategories': [
+          _id: ObjectId('5d2f7e66a5a47618d7080a0f'),
+          title: 'Детские товары и игрушки',
+          subcategories: [
             {
-              '_id': ObjectId('5d2f7e66a5a47618d7080a15'),
-              'title': 'Прогулки и детская комната',
+              _id: ObjectId('5d2f7e66a5a47618d7080a15'),
+              title: 'Прогулки и детская комната',
             },
           ],
         },
@@ -373,37 +351,31 @@ describe('email/order', () => {
 
       const products = [
         {
-          '_id': ObjectId('5d2f7e66a5a47618d7080a1f'),
-          'title': 'Коляска Adamex Barletta 2 in 1',
-          'description': 'description',
-          'category': categories[0]._id,
-          'subcategory': categories[0].subcategories[0]._id,
-          'images': [
-            'http://magazilla.ru/jpg_zoom1/598194.jpg',
-          ],
-          'price': 21230,
+          _id: ObjectId('5d2f7e66a5a47618d7080a1f'),
+          title: 'Коляска Adamex Barletta 2 in 1',
+          description: 'description',
+          category: categories[0]._id,
+          subcategory: categories[0].subcategories[0]._id,
+          images: ['http://magazilla.ru/jpg_zoom1/598194.jpg'],
+          price: 21230,
         },
         {
-          '_id': ObjectId('5d2f7e66a5a47618d7080a2f'),
-          'title': 'Коляска Peg Perego Si',
-          'description': 'description',
-          'category': categories[0]._id,
-          'subcategory': categories[0].subcategories[0]._id,
-          'images': [
-            'http://magazilla.ru/jpg_zoom1/164281.jpg',
-          ],
-          'price': 15818,
+          _id: ObjectId('5d2f7e66a5a47618d7080a2f'),
+          title: 'Коляска Peg Perego Si',
+          description: 'description',
+          category: categories[0]._id,
+          subcategory: categories[0].subcategories[0]._id,
+          images: ['http://magazilla.ru/jpg_zoom1/164281.jpg'],
+          price: 15818,
         },
         {
-          '_id': ObjectId('5d2f7e66a5a47618d7080a3f'),
-          'title': 'Коляска Adamex Barletta 3 in 1',
-          'description': 'description',
-          'category': categories[0]._id,
-          'subcategory': categories[0].subcategories[0]._id,
-          'images': [
-            'http://magazilla.ru/jpg_zoom1/1220903.jpg',
-          ],
-          'price': 26701,
+          _id: ObjectId('5d2f7e66a5a47618d7080a3f'),
+          title: 'Коляска Adamex Barletta 3 in 1',
+          description: 'description',
+          category: categories[0]._id,
+          subcategory: categories[0].subcategories[0]._id,
+          images: ['http://magazilla.ru/jpg_zoom1/1220903.jpg'],
+          price: 26701,
         },
       ];
 
@@ -433,22 +405,21 @@ describe('email/order', () => {
 
       await Order.insertMany(orders);
 
-      const {data} = await request({
+      const { data } = await request({
         method: 'get',
         url: serverURL,
         headers: {
-          'Authorization': `Bearer ${token}`,
+          Authorization: `Bearer ${token}`,
         },
       });
 
       expect(data.orders, 'ключ orders в ответе должел быть массивом').to.be.an('array');
       expect(data.orders, 'в ответе должно быть 2 заказа').to.have.lengthOf(2);
-      expect(data.orders, 'ответ должен содержать только заказы текущего пользователя')
-        .to.satisfy(() => data.orders.every((order) => order.user = user.id));
+      expect(data.orders, 'ответ должен содержать только заказы текущего пользователя').to.satisfy(() => data.orders.every(order => (order.user = user.id)));
     });
 
     it('вернуть ошибку со статусом 401 если пользователь не авторизован', async () => {
-      const {status} = await request({
+      const { status } = await request({
         method: 'post',
         url: serverURL,
       });
